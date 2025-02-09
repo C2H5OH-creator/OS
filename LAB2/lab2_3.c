0f58d7bf5de194b8eed4e998e99cf175427eb9ee
C2H5OH-creator <n.f.mikhaylov@gmail.com>
2025-02-09 16:35:25 +0300

    Вторая лаба сделана

---
+++ LAB2/lab2_3.c
@@ -0,0 +1,84 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+int flag1 = 1, flag2 = 2;
+pthread_mutex_t mutex;
+
+struct timespec tp;
+void* pthread_1(void* arg) {
+    printf("Поток 1 начал работу\n");
+    int* flag1 = (int*) arg;
+    while(*flag1) {
+	while (1) {
+	    clock_gettime(CLOCK_REALTIME, &tp);
+	    tp.tv_sec += 1;
+	    int rv = pthread_mutex_timedlock(&mutex, &tp);
+	    if (rv == 0) { break; }
+        }
+        for(int i = 0; i < 10; i++){
+	    printf("1");
+            fflush(stdout);
+            sleep(1);
+	}
+	sleep(1);
+	pthread_mutex_unlock(&mutex);
+    }
+
+    printf("Поток 1 закончил работу\n");
+    pthread_exit((void*)1);
+}
+
+void* pthread_2(void* arg) {
+    printf("Поток 2 начал работу\n");
+    int* flag2 = (int*) arg;
+    while(*flag2) {
+	while (1) {
+	    clock_gettime(CLOCK_REALTIME, &tp);
+	    tp.tv_sec += 1;
+	    int rv = pthread_mutex_timedlock(&mutex, &tp);
+	    if (rv == 0) { break; }
+        }
+        for(int i = 0; i < 10; i++){
+	    printf("2");
+            fflush(stdout);
+            sleep(1);
+	}
+	pthread_mutex_unlock(&mutex);
+	sleep(1);
+    }
+
+    printf("Поток 2 закончил работу\n");
+    pthread_exit((void*)2);
+
+}
+
+int main() {
+    pthread_t thread_id_1, thread_id_2;
+    void *exitcode1, *exitcode2;
+    pthread_mutex_init(&mutex, NULL);
+
+
+    printf("Основная программа начала работу\n");
+
+    pthread_create(&thread_id_1, NULL, pthread_1, &flag1);
+    pthread_create(&thread_id_2, NULL, pthread_2, &flag2);
+    printf("Программа ожидает нажатия клавиши\n");
+    getchar();
+
+    flag1 = 0;
+    flag2 = 0;
+
+    pthread_join(thread_id_1, &exitcode1);
+    pthread_join(thread_id_2, &exitcode2);
+
+    pthread_mutex_destroy(&mutex);
+
+    printf("Основная программа завершила работу\n");
+    return 0;
+
+}
---
+++ LAB2/labs.sh
@@ -0,0 +1,3 @@
+gcc lab2_1.c -o lab2_1
+gcc lab2_2.c -o lab2_2
+gcc lab2_3.c -o lab2_3
