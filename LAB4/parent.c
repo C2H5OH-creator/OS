ed24ea579a726535008f14996cfa6ee8d5c3edbb
C2H5OH-creator <n.f.mikhaylov@gmail.com>
2025-02-22 15:12:44 +0300

    Изменил запуск дочернего процесса, изменил логику работы агрументов, передаваемых в дочерний процесс

--- LAB4/parent.c
+++ LAB4/parent.c
@@ -4,6 +4,12 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
+    // Временно устанавливаем значение текущей директории, как переменную PATH
+    char *old_path = getenv("PATH");
+    char new_path[1024];
+    snprintf(new_path, sizeof(new_path), ".:%s", old_path);
+    setenv("PATH", new_path, 1);
+
     printf("Родительский процесс: PID = %d, PPID = %d\n", getpid(), getppid());
     pid_t pid = fork();
     if (pid == -1) {
@@ -11,25 +17,16 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     else if (pid == 0) {
-        char* childs_name = "./child";
-        char *args[] = {"Пример аргумента №1 переданного в дочерний процесс",
-                        "Пример аргумента №2 переданного в дочерний процесс",
-                        "Пример аргумента №3 переданного в дочерний процесс",
-                        "Пример аргумента №4 переданного в дочерний процесс",
-                        NULL};
-        execvp(childs_name, args);
+        char* childs_name = "child";
+        execvp(childs_name, argv);
         perror("execvp");
         exit(1);
     }
     else {
         int status = 0;
-        //const char frames[] = {'\\', '|', '/', '-'};
-        //unsigned short frames_index = 0;
         while (waitpid(pid, &status, WNOHANG) == 0) {
-            //printf("\rЖдём... %c", frames[frames_index]);
             printf("\rЖдём... ");
             fflush(stdout);
-            //frames_index = (frames_index++) % 4;
             usleep(200000);
         }
         if (WIFEXITED(status)) {
@@ -39,5 +36,7 @@ int main(int argc, char *argv[]) {
             printf("\nДочерний процесс завершился некорректно\n");
         }
     }
+    // Возвращаем старую переменную PATH как было
+    setenv("PATH", old_path, 1);
     return 0;
 }
